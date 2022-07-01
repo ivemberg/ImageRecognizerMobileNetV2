6dea0bb49060fc373333473e6c69b6c96423fe45
i.bettini <i.bettini@gmail.com>
2022-07-01 17:09:18 +0200

    Initial Commit

---
+++ ExampleML/ExampleMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleMLApp.swift
+//  ExampleML
+//
+//  Created by Ivo Junior Bettini on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
